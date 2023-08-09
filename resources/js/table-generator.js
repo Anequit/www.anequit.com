b7a060fbe5101f7632403b289fb7db2cbd56bc82
Bailey <Anequit@gmail.com>
2023-08-09 07:00:04 -0400

    Revert "commit"
    This reverts commit 9e38a5c7a109be8745cc3868dbce773f604b6a33.

--- resources/js/table-generator.js
+++ resources/js/table-generator.js
@@ -1,9 +1,9 @@
-let table: HTMLPreElement;
-let depth: HTMLInputElement;
+let table;
+let depth;
 
 window.onload = function () {
-    table = document.getElementById("table") as HTMLPreElement;
-    depth = document.getElementById("depth") as HTMLInputElement;
+    table = document.getElementById("table");
+    depth = document.getElementById("depth");
 
     // Execute a function when the user presses a key on the keyboard
     depth.addEventListener("keypress", function (event) {
@@ -12,41 +12,42 @@ window.onload = function () {
             // Cancel the default action, if needed
             event.preventDefault();
             // Trigger the button element with a click
-            (document.getElementById("generate") as HTMLButtonElement).click();
+            document.getElementById("generate").click();
         }
     });
 
-    let depthParameter: number | null = getURLParameter("depth");
+    let depthParameter = getURLParameter("depth");
 
     if (depthParameter != null) {
         if (validateDepth(depthParameter)) {
-            depth.value = String(depthParameter);
+            depth.value = depthParameter;
             updateTable(depthParameter);
         } else {
-            window.location.replace(window.location.href.split('?')[0]);
+            window.location = window.location.href.split('?')[0];
         }
+
     }
 }
 
-function getURLParameter(param: string): number | null {
+function getURLParameter(param) {
     let queryString = window.location.search.substring(1); // remove the "?" character
     let params = queryString.split('&'); // split by "&"
 
     for (let i = 0; i < params.length; i++) {
         let pair = params[i].split('='); // split by "="
         if (pair[0] == param) {
-            return Number(pair[1]); // return the value of the parameter
+            return pair[1]; // return the value of the parameter
         }
     }
 
     return null; // parameter not found
 }
 
-function centerStr(str: string, maxLength: number): string {
+function centerStr(str, maxLength) {
     return str.padStart(Math.ceil((str.length + maxLength) / 2), " ").padEnd(maxLength, " ");
 }
 
-function validateDepth(value: number): boolean {
+function validateDepth(value) {
     if (value > Number(depth.max) || value < Number(depth.min)) {
         console.log("false");
         return false;
@@ -55,7 +56,7 @@ function validateDepth(value: number): boolean {
     return true;
 }
 
-function updateTable(value: number) {
+function updateTable(value) {
     // clear previous table
     table.innerHTML = "";
 
